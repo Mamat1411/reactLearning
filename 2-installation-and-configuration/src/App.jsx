3797a63fcc7371d7fe152fe5935408c1254ded5f
Mamat1411 <muhammadmujahid14@gmail.com>
2024-05-28 15:53:36 +0700

    Changing H1

--- 2-installation-and-configuration/src/App.jsx
+++ 2-installation-and-configuration/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h2>Congrats🎊 You are Beginning to Learn and Use React The Right Way</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
